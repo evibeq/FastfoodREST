d4b2c91618f075b3a86c98e044dd91256bba243a
evibeq <evandro96@outlook.it>
2020-08-07 18:51:35 +0200

    a

--- routes/users.js
+++ routes/users.js
@@ -44,15 +44,11 @@ const userRoutes = (app, fs) => {
                 throw err;
             }
 
-            /*const userId = req.params["id"];
-
-            readid = data[userId];
-            console.log(JSON.stringify(data[userId.toString()]));*/
-
             const userId = req.params["id"];
             const ciao = JSON.parse(data);
-            
-            console.log(ciao);
+
+            console.log(typeof data);
+            console.log(typeof ciao);
 
             console.log(ciao[userId]);
 
