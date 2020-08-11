23611a1cdd174d67c1700abdc414dfd90180341b
evibeq <evandro96@outlook.it>
2020-08-11 16:14:38 +0200

    a

--- routes/recensioni.js
+++ routes/recensioni.js
@@ -108,7 +108,7 @@ const recensioniRoutes = (app, fs) => {
 
             data["contatore"]++;
 
-            req.body["id_recensione"] = toString(data["contatore"]);
+            req.body["id_recensione"] = json.stringify(data.["contatore"]);
             data["recensioni"].push(req.body);
 
             writeFile(JSON.stringify(data, null, 2), () => {
