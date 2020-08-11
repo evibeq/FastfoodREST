7d3b5810dd8384e2d2b80cd821255b055232ade2
evibeq <evandro96@outlook.it>
2020-08-11 09:04:01 +0200

    a

---
+++ routes/recensioni.js
@@ -0,0 +1,107 @@
+const recensioniRoutes = (app, fs) => {
+
+    // variables
+    const dataPath = './data/recensioni.json';
+
+    const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
+        fs.readFile(filePath, encoding, (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            callback(returnJson ? JSON.parse(data) : data);
+        });
+    };
+
+    const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
+
+        fs.writeFile(filePath, fileData, encoding, (err) => {
+            if (err) {
+                throw err;
+            }
+
+            callback();
+        });
+    };
+
+    // READ
+    app.get('/recensioni', (req, res) => {
+        fs.readFile(dataPath, 'utf8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            res.send(JSON.parse(data));
+        });
+    });
+
+    // READ ID
+    app.get('/recensioni/:id', (req, res) => {
+        fs.readFile(dataPath, 'utf8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            const id = req.params["id"];
+            const obj = JSON.parse(data);
+
+            var index = obj["id"].findIndex(function (item, i) {
+                return item.id_recensione === id
+            });
+
+            res.send(obj["id"][index]);
+        });
+    });
+
+    // CREATE
+    app.post('/recensioni', (req, res) => {
+
+        readFile(data => {
+
+            var index = data["recensioni"].findIndex(function (item, i) {
+                return item.id_recensione === req.body["id"]
+            });
+
+            if (index == -1){
+                const lastElement = data["recensioni"].length;
+                data["recensioni"][lastElement] = req.body;  
+            }
+
+            writeFile(JSON.stringify(data, null, 2), () => {
+                if (index == -1){
+                    res.status(201).send(`Aggiunta nuova Recensione ${req.body["id"]}`);
+                }else{
+                    res.status(200).send(`Recensione ${req.body["id"]} già esiste`);
+                }
+            });
+        },
+            true);
+    });
+
+    // DELETE
+    app.delete('/recensioni/:id', (req, res) => {
+
+        readFile(data => {
+
+            var index = data["recensioni"].findIndex(function (item, i) {
+                return item.id_recensione === req.params["id"]
+            });
+
+            if (index > -1){
+                data["recensioni"].splice(index, 1);
+            }
+
+            writeFile(JSON.stringify(data, null, 2), () => {
+                if (index == -1){
+                    res.status(200).send(`Recensione ${req.params["id"]} Non Esiste`);
+                } else {
+                    res.status(200).send(`Recensione ${req.params["id"]} Eliminata`);
+                }
+            });
+        },
+            true);
+    });
+
+};
+
+module.exports = recensioniRoutes;
