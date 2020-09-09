85fbfe40e2ba6018ae0dbd7ecef8764923753883
ciob <vasile.c99@gmail.com>
2020-09-09 11:28:20 +0200

    login

--- routes/login.js
+++ routes/login.js
@@ -6,7 +6,7 @@ const loginRoutes = (app, fs) => {
 
             // Funzione READFILE
             const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
-                fs.readFile(filePath, encoding, (err, data) => {
+                fs.readFileSync(filePath, encoding, (err, data) => {
                     if (err) {
                         throw err;
                     }
@@ -18,7 +18,7 @@ const loginRoutes = (app, fs) => {
             // Funzione WRITEFILE
             const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
         
-                fs.writeFile(filePath, fileData, encoding, (err) => {
+                fs.writeFileSync(filePath, fileData, encoding, (err) => {
                     if (err) {
                         throw err;
                     }
