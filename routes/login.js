25a676f35c646ab9754e2d8d1f5fbfc0d3f7e895
ciob <vasile.c99@gmail.com>
2020-09-09 11:20:18 +0200

    login

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,29 @@ const loginRoutes = (app, fs) => {
 
     const bcrypt = require('bcrypt');
 
+            // Funzione READFILE
+            const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
+                fs.readFile(filePath, encoding, (err, data) => {
+                    if (err) {
+                        throw err;
+                    }
+        
+                    callback(returnJson ? JSON.parse(data) : data);
+                });
+            };
+        
+            // Funzione WRITEFILE
+            const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
+        
+                fs.writeFile(filePath, fileData, encoding, (err) => {
+                    if (err) {
+                        throw err;
+                    }
+        
+                    callback();
+                });
+            };
+
     const users = []
 
     app.get('/users', (req, res) => {
@@ -11,11 +34,20 @@ const loginRoutes = (app, fs) => {
     })
 
     app.post('/users', async (req, res) => {
-        try {
-            const hashedPassword = await bcrypt.hash(req.body.password, 10)
-            const user = { user: req.body.user, password: hashedPassword }
-            users.push(user)
-            res.status(201).send()
+        try { 
+
+            readFile(data => {
+
+                const hashedPassword = await bcrypt.hash(req.body.password, 10)
+                const user = { user: req.body.user, password: hashedPassword }
+                login.utenti.push(user)
+    
+                writeFile(JSON.stringify(data, null, 2), () => {
+                    res.status(200).send({ messaggio: "Ordine ricevuto" })
+                });
+            },
+                true);
+
         } catch {
             res.status(500).send()
         }
@@ -36,30 +68,8 @@ const loginRoutes = (app, fs) => {
             res.status(500).send()
         }
     })
+    
     /*
-        // Funzione READFILE
-        const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
-            fs.readFile(filePath, encoding, (err, data) => {
-                if (err) {
-                    throw err;
-                }
-    
-                callback(returnJson ? JSON.parse(data) : data);
-            });
-        };
-    
-        // Funzione WRITEFILE
-        const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
-    
-            fs.writeFile(filePath, fileData, encoding, (err) => {
-                if (err) {
-                    throw err;
-                }
-    
-                callback();
-            });
-        };
-    
     
         // READ - GET /clienti 
         app.get('/clienti', (req, res) => {
