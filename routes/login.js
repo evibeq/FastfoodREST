21fb6d90de6b192d04e88f315542a369b0ff8417
evibeq <evandro96@outlook.it>
2020-09-09 12:13:18 +0200

    bcrypt hash

--- routes/login.js
+++ routes/login.js
@@ -29,25 +29,27 @@ const loginRoutes = (app, fs) => {
 
     const users = []
 
+    async function passwordHashed(password) {     
+        return await bcrypt.hash(password, 10)
+    }
+
     app.get('/users', (req, res) => {
         res.json(users)
     })
 
     app.post('/users', (req, res) => {
 
-        bcrypt.hash(req.body.password, 10).then(function(hash) {
-            // Store hash in your password DB.
             readFile(data => {
 
-                const user = { user: req.body.user, password: hash }
+                const hashedPassword = passwordHashed(req.body.password)
+                const user = { user: req.body.user, password: hashedPassword }
                 login.utenti.push(user)
     
                 writeFile(JSON.stringify(data, null, 2), () => {
-                    res.status(200).send({ messaggio: "User aggiunto" })
+                    res.status(200).send({ messaggio: "Ordine ricevuto" })
                 });
             },
                 true);
-        });
     })
 /*
     app.post('/users/login', async (req, res) => {
